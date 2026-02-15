0a12b3bd735090c938ab64650b012b1e54725b52
Atharv Deepak Yadav <atharv@gmail.com>
2026-02-15 11:26:52 +0530

    added jwt decoding for wix

--- server.js
+++ server.js
@@ -3,36 +3,31 @@ const jwt = require("jsonwebtoken");
 
 const app = express();
 
-// Wix sends RAW body
 app.use(express.raw({ type: "*/*" }));
 
-app.post("/v1/calculate-additional-fees", (req, res) => {
+app.post("/", (req, res) => {
   try {
     const token = req.body.toString();
-
-    // Decode Wix JWT payload
     const decoded = jwt.decode(token);
 
     console.log("Decoded Wix Payload:", decoded);
 
-   res.json({
-  additionalFees: [
-    {
-      code: "service-fee",
-      name: "Service Fee",
-      taxable: false,
-      amount: {
-        value: "39.00",
-        currency: "USD"
-      }
-    }
-  ]
-});
-
-
+    res.json({
+      additionalFees: [
+        {
+          code: "service-fee",
+          name: "Service Fee",
+          taxable: false,
+          amount: {
+            value: "39.00",
+            currency: "USD"
+          }
+        }
+      ]
+    });
 
   } catch (err) {
-    console.error("Error decoding token:", err);
+    console.error("Error:", err);
     res.status(400).send("Error");
   }
 });
